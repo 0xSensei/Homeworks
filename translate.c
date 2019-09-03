a09e063561d319d27095612fdf82884f65815f41
Abdulaziz <44272893+0xSensei@users.noreply.github.com>
2019-09-04 02:41:53 +0300

    Update translate.c

--- translate.c
+++ translate.c
@@ -6,117 +6,149 @@
 #define SIZE 10
 
 void TranslateFromSMS(char* dest, char* src);
-void TranslateToSMS(char* dest,char* src);
+void TranslateToSMS(char* dest, char* src);
 
 
 
 
-char* abb[] = {
-    "AFK",
-    "IMO",
-    "FISH",
-    "BRB",
-    "IMO",
-    "GG",
-    "BG",
-    "OMG",
-    "OMW",
-    "IMO"
-
-};
-
-char* real[] ={
-    "Away From Keyboard",
-    "In My Opinion",
-    "First In, Still Here",
-    "Be Right Back",
-    "In My Opinion",
-    "Good Game",
-    "Bad Game",
-    "Oh My GOD",
-    "Om My Way",
-    "In My Op"
+const char* abb[SIZE] = {
+	"AFK",
+	"IMO",
+	"FISH",
+	"BRB",
+	"FBI",
+	"GG",
+	"BG",
+	"OMG",
+	"OMW",
+	"KILL"
 
 };
 
+const char* real[SIZE] = {
+	"Away From Keyboard",
+	"In My Opinion",
+	"First In, Still Here",
+	"Be Right Back",
+	"Fedral investigation inteliangnce",
+	"Good Game",
+	"Bad Game",
+	"Oh My GOD",
+	"Om My Way",
+	"COPS!!"
+
+};
+
+int compare(const void * a, const void * b) {
+	return (*(int*)a - *(int*)b);
+}
+
+int main(void) {
 
 
-int main(void){
+	int choice = 0;
+	char newline = 0;
 
+	puts("Please enter a choice: \n1: sms to natural \n2: natural to sms ");
+	// scanf("%d", &choice);
+	char Dest[1000] = { 0 };
 
-    int choice = 0;
-    char newline =0;  
+	scanf("%d", &choice);
+	getc(stdin);
+	char sms_language[170] = { 0 };
+	char natural_language[300] = { 0 };
+	switch (choice)
+	{
+	case 1: // parse SMS language
 
-    puts("Please enter a choice: 1 -> sms to natural, 2: natural to sms ");
-    // scanf("%d", &choice);
-    char Dest[1000] ={0};
+		puts("Enter your sms message");
+		
+		fgets(sms_language, 170, stdin);
+		TranslateFromSMS(Dest, sms_language);
+		printf("%s", Dest);
+		break;
+	case 2: // parse Natural Language
 
-    scanf("%d", &choice);
-    getc(stdin);
+		puts("Enter your natural message");
+		
+		fgets(natural_language, 299, stdin);
 
-    switch (choice)
-    {
-    case 1: // parse SMS language
-        
-        puts("Enter your sms message");
-        char sms_language[170] = {0};
-        fgets(sms_language,170,stdin);
-        TranslateFromSMS(Dest, sms_language);
-
-        break;
-    case 2: // parse Natural Language
-    
-        puts("Enter your natural message");
-        char natural_language[300] = {0};
-        scanf("%299s", natural_language);
-        TranslateToSMS(Dest, natural_language);
-        break;
-    default:
-        break;
-    }
+		TranslateToSMS(Dest, natural_language);
+		printf("%s", Dest);
+		break;
+	default:
+		break;
+	}
 
 
 }
 
 
-void TranslateFromSMS(char* dest, char* src){
+void TranslateFromSMS(char* dest, char* src) {
 
-   
-    char *need_to_edit = NULL;
-    int counter = 1;
-    char * token = strtok(src," ");
 
-    while(token != NULL){
+	char *need_to_edit = NULL;
+	int counter = 1;
+	char * token = strtok(src, " ");
 
-        printf("the token is :%s\n", token);
-        for (size_t i = 0; i < SIZE; i++)
-        {
-            if(!strcmp(token,abb[i])){
-                    strcat(dest,real[i]);
-                    counter = 0;
-                    break;
-            }
-            
-        }
-        if(counter){
-            strcat(dest,token);
-        }
-        counter =1;
-        strcat(dest," ");
-        
+	while (token != NULL) {
 
-        token = strtok(NULL," ");
-    }
+		printf("the token is :%s\n", token);
+		for (size_t i = 0; i < SIZE; i++)
+		{
+			if (!strcmp(token, abb[i])) {
+				strcat(dest, real[i]);
+				counter = 0;
+				break;
+			}
 
-    puts(" ");
-    printf("%s", dest);
-    
+		}
+		if (counter) {
+			strcat(dest, token);
+		}
+		counter = 1;
+		strcat(dest, " ");
+
+
+		token = strtok(NULL, " ");
+	}
+
+	puts(" ");
+
 
 }
-void TranslateToSMS(char* dest,char* src){
+void TranslateToSMS(char* dest, char* src) {
+	// cut string for
 
-    printf("%s", dest);
-    printf("%s", src);          
+	// parse text and find the index's of want to be changed words into an array
+	// loop indexs and place the abb, 
+	//int indexs[SIZE] = { 900 ,900 ,900 ,900 ,900 ,900 ,900 ,900 ,900 ,900 };
+	
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		int counter = 0;
+		char * str_ = strstr(src+counter, real[i]);
+		while (str_)
+		{	// Away From Keyboard In My Opinion kk what is wrong Oh My GOD ->  \0way From Keyboard \0n My Opinion kk what is wrong \0h My GOD 
+			char tmp = *str_;
+			*str_ = '\0';
+			strcat(dest, src);
+			strcat(dest, abb[i]);
+			
+			*str_ = tmp;
+			counter = ( str_ -src) + strlen(real[i]);
+			strcat(dest, src + counter);
+			strcpy(src, dest);
+			memset(dest, 0, 300);
+			str_ = strstr(src+counter, real[i]);
+		}
+		
+	}
+	
+	
+	strcpy(dest, src);
+	puts(" ");
+
 
 
 }
